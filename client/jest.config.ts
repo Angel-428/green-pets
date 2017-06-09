65dea190d3174d103628af67d2df0a93953b5123
Angel-428 <minato20010119@gmail.com>
2017-06-09 12:00:00 -0400

    Update Jest

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,11 +1,4 @@
-// import '@testing-library/jest-dom';
-// export const preset = 'ts-jest';
-// export const testEnvironment = 'node';
-// export const testMatch = [
-//   "**/__tests__/**/*.ts?(x)",
-//   "**/?(*.)+(spec|test).ts?(x)"
-// ];
-// export const setupFilesAfterEnv = ['@testing-library/jest-dom'];
+
 
 import { defaults as tsjPreset } from 'ts-jest/presets';
 
